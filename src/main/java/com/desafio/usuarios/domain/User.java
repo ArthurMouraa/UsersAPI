f96c16718620cc24eb037df4a504439206d44d51
ArthurMouraa <arthurmoura090@gmail.com>
2025-09-09 16:44:21 -0300

    entity user created

--- src/main/java/com/desafio/usuarios/domain/User.java
+++ src/main/java/com/desafio/usuarios/domain/User.java
@@ -1,4 +1,79 @@
 package com.desafio.usuarios.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.Instant;
+
 public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String role;
+
+    @JsonProperty("is_active")
+    private boolean isActive;
+
+    @JsonProperty("created_at")
+    private Instant createdAt;
+
+    public User(){
+
+    }
+    public User(int id, String name, String email, String role, boolean isActive, Instant createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.isActive = isActive;
+        this.createdAt = createdAt;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean active) {
+        this.isActive = active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
 }
