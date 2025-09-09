95cd3f308aecaa909ab8c42a412888928a6e49a7
ArthurMouraa <arthurmoura090@gmail.com>
2025-09-09 17:16:40 -0300

    feat:automated test for created repository

--- src/test/java/com/desafio/usuarios/domain/repository/UserRepositoryTest.java
+++ src/test/java/com/desafio/usuarios/domain/repository/UserRepositoryTest.java
@@ -1,16 +1,28 @@
 package com.desafio.usuarios.domain.repository;
 
+import com.desafio.usuarios.domain.entity.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRepositoryTest {
 
-    @Test
-    void findAll() {
+
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        userRepository = new UserRepository();
     }
 
     @Test
-    void findById() {
+    void deveCarregarUsuariosDoJson() {
+        List<User> users = userRepository.findAll();
+        assertNotNull(users);
+        assertFalse(users.isEmpty());
     }
 }
